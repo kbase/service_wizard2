d531e341f6b81d004c6b79788c44a128ad122e96
Boris <bio-boris@github.com>
2023-10-19 18:29:38 -0500

    Update .env

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Helper script to run tests
 
-PYTHONPATH=.:src pipenv run pytest --cov=src --cov-report term-missing --cov-fail-under=99 --cov-report=xml:coverage.xml  -W ignore::DeprecationWarning test
+PYTHONPATH=.:src pipenv run pytest --ignore=src/clients/CatalogClient.py --ignore=src/clients/baseclient.py --cov=src --cov-report term-missing --cov-fail-under=99 --cov-report=xml:coverage.xml -W ignore::DeprecationWarning test
