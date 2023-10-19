b579c46dab2cdb4030202091fd8f8ddd0cef5360
Boris <bio-boris@github.com>
2023-10-19 16:25:57 -0500

    Update .env

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Helper script to run tests
+pipenv shell
 PYTHONPATH=.:src pytest --cov=src --cov-report term-missing --cov-fail-under=99 --cov-report=xml:coverage.xml  -W ignore::DeprecationWarning test
