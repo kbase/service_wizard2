f22768681f7f2f3e3feae0f6bc73e9a8cc444e3c
Boris <bio-boris@github.com>
2023-10-19 16:49:02 -0500

    Update .env

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+
+if [ -z "$GITHUB_ACTION" ]; then
+    echo "This step is intended to be run from Github Actions"
+    pip install pipenv
+    rm Pipfile.lock
+    pipenv install --dev
+    pipenv sync --system --dev
+    exit 0
+fi
+
+
+
 if [[ -n $VIRTUAL_ENV ]]; then
     echo "Pipenv shell is  activated and ready for updates"
-    rm Pipfile.lock
+
     pipenv install --dev
     pipenv sync
     echo "Updated dependencies for: `which python`"
